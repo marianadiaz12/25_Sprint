8f15c8d142ad702895e110407172a9a4eb1c7f66
marianadiaz12 <mariana.diazes@gmail.com>
2016-12-02 08:08:14 -0600

    Spring Came.v2

--- js/main.js
+++ js/main.js
@@ -9,11 +9,4 @@ $(document).ready(function(){
 	$('input').focus(function(){
         $(this).css('outline-color', '#FF0000');
      });
-
-	$('h1').mouseenter(function(){
-		$(this).animate({fontSize: '7em'}, 'slow');
-	});
-	$('h1').mouseleave(function(){
-		$(this).animate({fontSize: '4em'}, 'slow');
-	});
 });
